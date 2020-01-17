3483126769c4dcb05872bb3b3e676b90207dade6
Luke de Waal <lr.de.waal.01@gmail.com>
2020-01-17 11:31:51 +0100

    .

--- Arduino FIles/robot/robot.ino
+++ Arduino FIles/robot/robot.ino
@@ -1,33 +1,31 @@
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 #include <stdio.h>
+#include <std_msgs/String.h>
 
+// Yellow LED
+const int LED_pin = 13;
 
 // Ultrasonwsic Sensor Pins
-const int trigger_pin = 11; //23;
-const int echo_pin = 10; //22;
+const int trigger_pin = 23;
+const int echo_pin = 22;
 const float stopping_distance = 8.0; // cm
 const float critical_distance = 4.0; // cm
 
 // Motor Pins
 // Left
-const int rev_1 = 9;//7;
-const int en_1  = 7;//24;
-const int fwd_1 = 6;//6;
+const int rev_1 = 7;
+const int en_1  = 24;
+const int fwd_1 = 6;
 
 // Right
-const int rev_2 = 3;//3;
-const int en_2  = 4;//25;
-const int fwd_2 = 5;//2;
+const int rev_2 = 3;
+const int en_2  = 25;
+const int fwd_2 = 2;
 
 // Bluetooth Pins
 const int BT_TX = 18;
 const int BT_RX = 19;
-
-//// Joystick Pins
-//const int SW_pin = 2;
-//const int X_pin = 3;
-//const int Y_pin = 4;
 
 // Further Useful Variables
 float dist = 0;
@@ -38,44 +36,79 @@ char input;
 
 // ROS Stuff
 ros::NodeHandle nh;
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
 
-void velCallback(const geometry_msgs::Twist& vel){
-  Serial.print((float)vel.linear.x);
-  Serial.println();
+int x;
+int y; 
+
+char buffer[100];
+
+void velCallback(  const geometry_msgs::Twist& vel)
+    {
+        x = (int)(vel.linear.x*1000);
+        y = vel.linear.y;
+
+        nh.loginfo("Method Called");
+        snprintf ( buffer, 100, "X = %d", x);
+        nh.loginfo(buffer);
+        
+        if (x > 1) //Forward move.
+          {
+          //set_low(fwd_1, fwd_2);
+          set_low(rev_1, rev_2);
+          forward();
+          nh.loginfo("LED");
+          digitalWrite(LED_pin, HIGH);
+          delay(250);
+          digitalWrite(LED_pin, LOW);
+          }
+        else if (x < -1)   //Backward move
+        {
+           reverse();
+        }
+        else 
+        {
+           hold();
+        }
 }
 
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", velCallback);
 
-void setup(){
-  Serial.begin(9600);
-  set_inputs(fwd_1, fwd_2, rev_1, rev_2, en_1, en_2, BT_RX, 
-              echo_pin);
-  set_outputs(BT_RX, trigger_pin);
 
-  //nh.initNode();
-  //nh.subscribe(sub);
+void setup(){
+
+  set_inputs( fwd_1, fwd_2, 
+              rev_1, rev_2, 
+              en_1, en_2, 
+              BT_RX, 
+              echo_pin, LED_pin);
+              
+  set_outputs(BT_RX, trigger_pin);
   
+  set_low(rev_1, rev_2);
+  set_low(fwd_1, fwd_2);
+  
+  nh.initNode();
+  nh.subscribe(sub);
+  digitalWrite(en_1, HIGH);
+  digitalWrite(en_2, HIGH);
 }
+
+
 
 void loop() {
   dist = distance();
-  Serial.print("~");
-  Serial.print(dist);
-  Serial.print("cm  -  ");
-  //nh.spinOnce();
+  snprintf ( buffer, 100, "D = %f", dist);
+  nh.loginfo(buffer);
+  //analogWrite(fwd_1, 255);
+  //analogWrite(fwd_2, 255);
   
-  if(free_path(dist)){
-    if(Serial.available()){
-      input = Serial.read();
-    }
-  }
-  else{
-    input = 'x';   
-  }
-  keyboard_to_movement(input);
-  delay(20);
+  nh.spinOnce();
   
 }
+
+
 
 template<typename T>
 void set_inputs(T first){
@@ -105,24 +138,20 @@ void set_low(const int pin_1, const int pin_2){
 }
 
 void forward(){
-
-  set_low(fwd_1, fwd_2);
-  set_low(rev_1, rev_2);
+  hold();
   digitalWrite(fwd_1, HIGH);
   digitalWrite(fwd_2, HIGH);
 }
 
 void turn_left(){
-
-  set_low(fwd_1, rev_2);
+  hold();
   digitalWrite(rev_1, HIGH);
   digitalWrite(fwd_2, HIGH);
 
 }
 
 void turn_right(){
-
-  set_low(fwd_2, rev_1);
+  hold();
   digitalWrite(rev_2, HIGH);
   digitalWrite(fwd_1, HIGH);
 }
@@ -137,7 +166,6 @@ void hold(){
 
   set_low(fwd_1, fwd_2);
   set_low(rev_1, rev_2);
-  set_low(en_1, en_2);
 }
 
 double microsecondsToCentimeters(double microseconds) {
@@ -162,15 +190,6 @@ float distance(){
   return cm;
 }
 
-bool emergency_stop(float distance){
-  
- if(distance <= critical_distance){
-    return true;
-  } else {
-    return false;
-  } 
-}
-
 bool free_path(float distance){
   
   if(distance <= stopping_distance){
@@ -184,63 +203,23 @@ void keyboard_to_movement(char input){
 
   if(input == 'w'){
     forward();
-    Serial.print("^");
+    //Serial.print("^");
   }
   else if(input == 's'){
     reverse();
-    Serial.print("v");
+    //Serial.print("v");
   }
   else if(input == 'a'){
     turn_left();
-    Serial.print("<");
+    //Serial.print("<");
   }
   else if(input == 'd'){
     turn_right();
-    Serial.print(">");
+    //Serial.print(">");
   }
   else {
     hold();
-    Serial.print("X");
+    //Serial.print("X");
   }  
-  Serial.println();
+  //Serial.println();
 }
-
-//void joystick_to_movement(){
-//
-//  read_joystick();
-//  
-//  if(joystick_X > minval_range && -minval_range < joystick_Y < minval_range){
-//    turn_right();
-//    Serial.print("Right");
-//  }
-//  else if(joystick_X < -minval_range && -minval_range < joystick_Y < minval_range){
-//    turn_left();
-//    Serial.print("Left");
-//  }
-//  else if(-minval_range < joystick_X < minval_range && joystick_Y > minval_range){
-//    forward();
-//    Serial.print("Forward");
-//  }
-//  else if(-minval_range < joystick_X < minval_range && joystick_Y < -minval_range){
-//    reverse();
-//    Serial.print("Backward");
-//  }
-//  else{
-//    hold();
-//    Serial.print("Holding");
-//    
-//  }
-//  Serial.println();
-//}
-//
-//void read_joystick(){
-//  joystick_X = ((float)analogRead(X_pin) - 512)/52.15;
-//  joystick_Y = ((float)analogRead(Y_pin) - 512)/-52.13;
-//}
-//
-//void joystick_input(){
-//  Serial.print(joystick_X);
-//  Serial.print(" - ");
-//  Serial.print(joystick_Y);
-//  Serial.println();
-//}
